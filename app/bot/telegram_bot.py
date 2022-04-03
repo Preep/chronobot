276060828b1e479ec2b9e18fddf416f789a3fd17
Valery Lazarev <lzrv@feip.co>
2022-04-03 22:55:32 +1000

    Add some TODOs

--- app/bot/telegram_bot.py
+++ app/bot/telegram_bot.py
@@ -1,5 +1,5 @@
 import telebot
-from telebot.types import Message
+from telebot import types as telebot_types
 import os
 from .parser import parse_and_sanitize
 
@@ -12,20 +12,20 @@ bot = telebot.TeleBot(token)
 
 
 @bot.message_handler(commands=['start'])
-def __start_handler(message: Message):
+def __start_handler(message: telebot_types.Message):
     bot.send_message(message.chat.id, 'Приветик я будущий бот, я пока что в разработке. Попробуй позже.')
 
 
 @bot.message_handler(content_types=['text'])
-def __text_handler(message: Message):
+def __text_handler(message: telebot_types.Message):
 
     parsed_message_dict = parse_and_sanitize(message.text)
     parsed_message_dict['chat_id'] = message.chat.id
 
     if parsed_message_dict['error'] is None:
-        bot.send_message(message.chat.id, str(parsed_message_dict))
-        # Show color menu
-        # Send parsed data and chosen color to IO handler
+        bot.send_message(message.chat.id, str(parsed_message_dict)) # TODO: Delete when DB handler is ready
+        # TODO: Add colors keyboard for event types as per chronometr method by Kamaeva
+        # TODO: Send parsed data and chosen color to IO handler
     else:
         bot.send_message(message.chat.id, parsed_message_dict['error'])
 
